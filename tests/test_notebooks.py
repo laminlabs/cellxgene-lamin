1ce54f7b78f0745292609eef22552af638b03ea1
Alex Wolf <f.alexander.wolf@gmail.com>
2023-11-24 10:14:06 +0100

    💚 Try to debug

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,4 +6,6 @@ import nbproject_test as test
 def test_notebooks():
     # assuming this is in the tests folder
     docs_folder = Path(__file__).parents[1] / "docs/"
-    test.execute_notebooks(docs_folder, write=True)
+    test.execute_notebooks(
+        docs_folder, write=True, print_cells=True, print_outputs=True
+    )
