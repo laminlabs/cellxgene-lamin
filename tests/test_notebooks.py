fb0dea421b1b78ecde79a2cf6c230f3afe5a51c4
zethson <lukas.heumos@posteo.net>
2024-03-27 14:46:01 +0100

    :art: Remove nbstriout
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -11,11 +11,9 @@ DOCS = Path(__file__).parent.parent / "docs/"
 
 def test_census():
     for filename in GROUPS["census"]:
-        print(filename)
         test.execute_notebooks(DOCS / filename, write=True)
 
 
 def test_validator():
     for filename in GROUPS["validator"]:
-        print(filename)
         test.execute_notebooks(DOCS / filename, write=True)
