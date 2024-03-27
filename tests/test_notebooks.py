66dbf0c426882fa01f5797a56e433eece023b37b
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-03-27 09:56:45 +0100

    💚 Fix

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,18 +1,9 @@
 from pathlib import Path
 
-import nbproject_test as test
-from noxfile import GROUPS
+import nbproject_test
 
-DOCS = Path(__file__).parent.parent / "docs/"
-
-
-def test_census():
-    for filename in GROUPS["census"]:
-        print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+notebook_dir = Path(__file__).parent.parent / "docs/"
 
 
-def test_validator():
-    for filename in GROUPS["validator"]:
-        print(filename)
-        test.execute_notebooks(DOCS / filename, write=True)
+def test_all_notebooks():
+    nbproject_test.execute_notebooks(notebook_dir)
