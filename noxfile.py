fbab7e668f45d0d28b0c5b576a9053664fc162c9
Lukas Heumos <lukas.heumos@posteo.net>
2024-03-27 11:21:13 -0700

    Fix notebook execution rendering (#39)
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -4,9 +4,10 @@ from laminci.nox import build_docs, login_testuser1, run_pre_commit
 
 nox.options.default_venv_backend = "none"
 
-GROUPS = {}
-GROUPS["census"] = ["query-census.ipynb"]
-GROUPS["validator"] = ["cellxgene.ipynb", "cellxgene-lamin-validator.ipynb"]
+GROUPS = {
+    "census": ["query-census.ipynb"],
+    "validator": ["cellxgene.ipynb", "cellxgene-lamin-validator.ipynb"],
+}
 
 
 @nox.session
@@ -23,16 +24,16 @@ def install(session: nox.Session, group: str) -> None:
     extra = ""
     if group == "census":
         extra = ",jupyter,aws"
-        session.run(*"pip install cellxgene-census".split())
+        session.run(*"uv pip install --system cellxgene-census".split())
     elif group == "validator":
         extra = ",jupyter,aws,zarr"
-        session.run(*"pip install cellxgene-schema".split())
-        session.run(*"pip install anndata==0.9.0".split())
-    session.run(*"pip install .[dev]".split())
+        session.run(*"uv pip install --system cellxgene-schema".split())
+        session.run(*"uv pip install --system anndata==0.9.0".split())
+    session.run(*"uv pip install --system .[dev]".split())
     session.run(
         "pip",
         "install",
-        f"lamindb[bionty{extra}] @ git+https://github.com/laminlabs/lamindb@release",
+        f"lamindb[bionty{extra}] @ git+https://github.com/laminlabs/lamindb@main",
     )
 
 
@@ -50,6 +51,17 @@ def build(session, group):
 @nox.session
 def docs(session):
     login_testuser1(session)
+    # TODO This is currently a hack because else the executed notebooks are not rendered!
+    extra = ",jupyter,aws,zarr"
+    session.run(*"uv pip install --system cellxgene-schema".split())
+    session.run(*"uv pip install --system anndata==0.9.0".split())
+    session.run(*"uv pip install --system .[dev]".split())
+    session.run(
+        "pip",
+        "install",
+        f"lamindb[bionty{extra}] @ git+https://github.com/laminlabs/lamindb@main",
+    )
+    session.run(*"pytest -s ./tests/test_notebooks.py::test_validator".split())
     session.run(*"lamin init --storage ./docsbuild --schema bionty".split())
-    build_docs(session, strict=False)
+    build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,22 +1,19 @@
-import sys
 from pathlib import Path
 
 import nbproject_test as test
 
-sys.path[:0] = [str(Path(__file__).parent.parent)]
-
-from noxfile import GROUPS
-
-DOCS = Path(__file__).parents[1] / "docs/"
+GROUPS = {
+    "census": ["query-census.ipynb"],
+    "validator": ["cellxgene.ipynb", "cellxgene-lamin-validator.ipynb"],
+}
+DOCS = Path(__file__).parent.parent / "docs/"
 
 
 def test_census():
     for filename in GROUPS["census"]:
-        print(filename)
         test.execute_notebooks(DOCS / filename, write=True)
 
 
 def test_validator():
     for filename in GROUPS["validator"]:
-        print(filename)
         test.execute_notebooks(DOCS / filename, write=True)
