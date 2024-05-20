659bbc782ea15f4843d778ae21cced3a51f13820
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-05-20 13:10:24 +0200

    🔖 Release 0.2.2

--- cellxgene_lamin/__init__.py
+++ cellxgene_lamin/__init__.py
@@ -14,7 +14,7 @@ This is the complete API reference:
    datasets
 """
 
-__version__ = "0.2.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.2.2"  # denote a pre-release for 0.1.0 with 0.1rc1
 
 from . import datasets
 from ._annotate import Annotate
