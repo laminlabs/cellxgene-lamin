3b24061b0f9a218a923c937850a15ad473e4b649
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2024-07-16 14:28:06 +0200

    🚨 Fix

--- cellxgene_lamin/_features.py
+++ cellxgene_lamin/_features.py
@@ -20,7 +20,9 @@ ACCESSORS = get_accessor_by_registry_(ln.Artifact)
 FEATURE_TO_ACCESSOR = {}
 for name in OBS_FEATURES.keys():
     feature = getattr(obs_features_records, name)
-    accessor = ACCESSORS.get(feature.dtype[feature.dtype.index('[') + 1 : feature.dtype.index(']')])
+    accessor = ACCESSORS.get(
+        feature.dtype[feature.dtype.index("[") + 1 : feature.dtype.index("]")]
+    )
     orm = getattr(ln.Artifact, accessor).field.model
     # TODO: ulabels are defined in the File model, improve this in LaminDB
     if orm == ln.Artifact:
@@ -38,7 +40,7 @@ def register_obs_featureset(artifacts):
             record = ln.Feature(name=name, dtype=f"cat[{registry}]").save()
             features_records.append(record)
         ln.save(features_records)
-        feature_set = ln.FeatureSet(features=features_records, name=f"{slot} metadata")
+        feature_set = ln.FeatureSet(features=features_records, name="obs metadata")
         feature_set.save()
 
     feature_set.artifacts.add(*artifacts, through_defaults={"slot": "obs"})
