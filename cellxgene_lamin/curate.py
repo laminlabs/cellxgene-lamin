807a0a102c40cfe79969eda81aef8945be334728
zethson <lukas.heumos@posteo.net>
2024-08-02 11:53:21 +0200

    :sparkles: Save collection explicitly now
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- cellxgene_lamin/curate.py
+++ cellxgene_lamin/curate.py
@@ -163,41 +163,6 @@ class Curate(AnnDataCurator):
 
         return super().validate(organism=self.organism)
 
-    def save_collection(
-        self,
-        artifact: Artifact | Iterable[Artifact],
-        name: str,
-        description: str | None = None,
-        reference: str | None = None,
-        reference_type: str | None = None,
-    ) -> Collection:
-        """Save a collection from artifact/artifacts.
-
-        Args:
-            artifact: One or several saved Artifacts.
-            name: Title of the publication.
-            description: Description of the publication.
-            reference: Accession number (e.g. GSE#, E-MTAB#, etc.).
-            reference_type: Source type (e.g. GEO, ArrayExpress, SRA, etc.).
-        """
-        collection = Collection(
-            artifact,
-            name=name,
-            description=description,
-            reference=reference,
-            reference_type=reference_type,
-        )
-        slug = ln_setup.settings.instance.slug
-        if collection._state.adding:
-            collection.save()
-        else:  # pragma: no cover
-            collection.save()
-            logger.warning(f"collection already exists in {colors.italic(slug)}!")
-        if ln_setup.settings.instance.is_remote:  # pragma: no cover
-            logger.print(f"go to https://lamin.ai/{slug}/collection/{collection.uid}")
-        self._collection = collection
-        return collection
-
     def to_cellxgene_anndata(
         self, is_primary_data: bool, title: str | None = None
     ) -> ad.AnnData:
