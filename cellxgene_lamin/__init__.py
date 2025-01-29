42c4cbdc8f6289d5b6ce79a5a31ffd9a445f599b
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-30 00:14:32 +0800

    🚚 Move Curator to lamindb

--- cellxgene_lamin/__init__.py
+++ cellxgene_lamin/__init__.py
@@ -9,16 +9,12 @@ This is the complete API reference:
 .. autosummary::
    :toctree: .
 
-   Curate
-   CellxGeneFields
    datasets
 """
 
 __version__ = "0.3.2"  # denote a pre-release for 0.1.0 with 0.1rc1
 
+from lamindb.curators import CellXGeneAnnDataCurator, CellxGeneFields
+
 from .curate import Curator
 from .dev import datasets
-from .fields import CellxGeneFields
-
-# backwards compat
-Curate = Curator
