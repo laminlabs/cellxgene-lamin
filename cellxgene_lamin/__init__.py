fc8059857d1f699d9f65aeec6a3209d11a3a1a23
Alex Wolf <f.alexander.wolf@gmail.com>
2025-01-30 00:22:01 +0800

    💚 Fix typo

--- cellxgene_lamin/__init__.py
+++ cellxgene_lamin/__init__.py
@@ -14,7 +14,7 @@ This is the complete API reference:
 
 __version__ = "0.3.2"  # denote a pre-release for 0.1.0 with 0.1rc1
 
-from lamindb.curators import CellXGeneAnnDataCurator, CellxGeneFields
+from lamindb.curators import CellxGeneAnnDataCurator, CellxGeneFields
 
 from .curate import Curator
 from .dev import datasets
