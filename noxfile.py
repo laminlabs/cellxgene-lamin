88491badf715dd33451c2fcfb9060d40579f282e
Alex Wolf <f.alexander.wolf@gmail.com>
2025-04-14 06:21:17 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -63,6 +63,6 @@ def docs(session):
         for path in Path(f"./docs_{group}").glob("*"):
             path.rename(f"./docs/{path.name}")
 
-    run(session, "lamin init --storage ./docsbuild --schema bionty")
+    run(session, "lamin init --storage ./docsbuild --modules bionty")
     build_docs(session, strict=True)
     upload_docs_artifact(aws=True)
