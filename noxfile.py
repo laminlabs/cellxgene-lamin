1723d85d8344cde0de3d767dccca71d6b6216a52
Lukas Heumos <lukas.heumos@posteo.net>
2025-07-02 14:29:35 +0200

    🎨 Fix CI and upgrade versions
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -33,10 +33,10 @@ def install(session: nox.Session, group: str) -> None:
     extras = ""
     if group == "census":
         extras = "bionty,jupyter"
-        run(session, "uv pip install --system tiledbsoma==1.15.0rc3")
+        run(session, "uv pip install --system tiledbsoma")
     elif group == "validator":
         extras = "bionty,jupyter,zarr"
-        run(session, "uv pip install --system tiledbsoma==1.15.0rc3")
+        run(session, "uv pip install --system tiledbsoma")
         run(session, "uv tool install cellxgene-schema==5.2.2")
     install_lamindb(session, branch="main", extras=extras)
     run(session, "uv pip install --system .[dev]")
