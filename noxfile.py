e55c17f0368426d9eada78c0166636b06dbf7b8e
Lukas Heumos <lukas.heumos@posteo.net>
2025-07-21 09:47:55 +0200

    🎨 Go main
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -38,7 +38,7 @@ def install(session: nox.Session, group: str) -> None:
             "uv pip install --system pronto tiledbsoma scanpy>=1.11.3",
         )  # scanpy pin to prevent scipy installation crashes
         run(session, "uv tool install cellxgene-schema==5.2.2")
-    install_lamindb(session, branch="feature/cellxgene_compat", extras=extras)
+    install_lamindb(session, branch="main", extras=extras)
     run(session, "uv pip install --system .[dev]")
 
 
