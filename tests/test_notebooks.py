60f8378a9f53aa8abed8546e08a0c72e61849963
Alex Wolf <f.alexander.wolf@gmail.com>
2026-05-07 14:08:36 +0200

    💚 Fix

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -14,6 +14,6 @@ def test_query():
         test.execute_notebooks(DOCS / filename, write=True)
 
 
-def test_validator():
+def test_curate():
     for filename in GROUPS["curate"]:
         test.execute_notebooks(DOCS / filename, write=True)
